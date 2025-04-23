e3e89c832cd46f41089950bee47da7c1c715c860
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-23 08:58:26 -0400

    Update TodoItem class as entity to database

--- src/main/java/io/github/jhairs2/todo_list/todo/model/TodoItem.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/model/TodoItem.java
@@ -1,6 +1,19 @@
 package io.github.jhairs2.todo_list.todo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Tasks")
 public class TodoItem {
+    @Id
+    @SequenceGenerator(name = "task_sequence", sequenceName = "task_sequence", allocationSize = 1)
+
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "task_sequence")
 
     private Long id;
     private String task;
