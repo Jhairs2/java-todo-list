d73d4012e82d494b435feeda0523e3f7bd312c6e
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-23 08:14:36 -0400

    Add path to controller and some test code

--- src/main/java/io/github/jhairs2/todo_list/todo/controller/TodoListController.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/controller/TodoListController.java
@@ -1,5 +1,20 @@
 package io.github.jhairs2.todo_list.todo.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.github.jhairs2.todo_list.todo.model.TodoItem;
+
+@RestController
+@RequestMapping(path = "/api/v1/todos")
 public class TodoListController {
+
+    @GetMapping
+    public List<TodoItem> getAllTodoList() {
+
+        return List.of(new TodoItem(1L, "Say Hi!"));
+
+    }
 
 }
