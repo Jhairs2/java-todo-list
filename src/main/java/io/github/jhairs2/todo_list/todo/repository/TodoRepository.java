f010c9ff82a9f9ecc52034f2289522d1b7fb42b7
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-23 09:08:25 -0400

    Add @repository annotation

--- src/main/java/io/github/jhairs2/todo_list/todo/repository/TodoRepository.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/repository/TodoRepository.java
@@ -1,5 +1,11 @@
 package io.github.jhairs2.todo_list.todo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.jhairs2.todo_list.todo.model.TodoItem;
+
+@Repository
 public interface TodoRepository extends JpaRepository<TodoItem, Long> {
 
 }
