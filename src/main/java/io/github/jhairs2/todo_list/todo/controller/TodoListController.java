a987ae79a29901cf6ef0761c88e7e0a2bd739670
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-24 08:36:17 -0400

    Create update todo function in controller class

--- src/main/java/io/github/jhairs2/todo_list/todo/controller/TodoListController.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/controller/TodoListController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,11 @@ public class TodoListController {
     @PostMapping
     public TodoItem addTodoToList(@RequestBody TodoItem task) {
         return this.todoService.addTodoToList(task);
+    }
+
+    @PutMapping(path = "/{taskId}")
+    public TodoItem updateTodoById(@PathVariable("taskId") Long id, @RequestBody TodoItem task) {
+        return this.todoService.updateTodoById(id, task);
     }
 
 }
