598860b3ae6691c2cb769e2ac8d1b73de4a4418e
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-24 06:48:05 -0400

    Create service class

--- src/main/java/io/github/jhairs2/todo_list/todo/controller/TodoListController.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/controller/TodoListController.java
@@ -1,19 +1,29 @@
 package io.github.jhairs2.todo_list.todo.controller;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.github.jhairs2.todo_list.todo.model.TodoItem;
+import io.github.jhairs2.todo_list.todo.repository.TodoRepository;
 
 @RestController
 @RequestMapping(path = "/api/v1/todos")
 public class TodoListController {
 
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoListController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
     @GetMapping
     public List<TodoItem> getAllTodoList() {
-
-        return List.of(new TodoItem(1L, "Say Hi!"));
+        return this.todoRepository.findAll();
 
     }
 
