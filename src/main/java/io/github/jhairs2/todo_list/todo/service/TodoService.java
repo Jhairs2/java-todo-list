cf6d8ba98352eb07032d09cc392d3ecb681cd353
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-24 06:57:24 -0400

    Define service class and add constructor and getter for list

--- src/main/java/io/github/jhairs2/todo_list/todo/service/TodoService.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/service/TodoService.java
@@ -1,5 +1,26 @@
 package io.github.jhairs2.todo_list.todo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.jhairs2.todo_list.todo.model.TodoItem;
+import io.github.jhairs2.todo_list.todo.repository.TodoRepository;
+
+@Service
 public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<TodoItem> getAllTodoList() {
+        return this.todoRepository.findAll();
+
+    }
 
 }
