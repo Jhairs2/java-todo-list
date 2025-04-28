16fcbcc905d916ef5b29c2058929b901139ebbcf
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-28 03:15:15 -0400

    Add function for returning project lists

--- src/main/java/io/github/jhairs2/todo_list/todo/controller/ProjectListController.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/controller/ProjectListController.java
@@ -1,5 +1,27 @@
 package io.github.jhairs2.todo_list.todo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.github.jhairs2.todo_list.todo.model.ProjectList;
+import io.github.jhairs2.todo_list.todo.service.ProjectListService;
+
+@RestController
+@RequestMapping(path = "/api/v1/projects")
 public class ProjectListController {
 
+    private final ProjectListService projectListService;
+
+    @Autowired
+    public ProjectListController(ProjectListService projectListService) {
+        this.projectListService = projectListService;
+    }
+
+    @GetMapping
+    public List<ProjectList> getAllProjectLists() {
+        return this.projectListService.getAllProjectLists();
+    }
 }
