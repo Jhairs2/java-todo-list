5be373179666688073ba22e8319940979dde635b
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-28 10:11:33 -0400

    Update ProjectListDTO with vars, constructor, getters, and setters

--- src/main/java/io/github/jhairs2/todo_list/todo/dto/ProjectListDTO.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/dto/ProjectListDTO.java
@@ -2,4 +2,27 @@ package io.github.jhairs2.todo_list.todo.dto;
 
 public class ProjectListDTO {
 
+    private Long id;
+    private String projectTitle;
+
+    public ProjectListDTO(Long id, String projectTitle) {
+        this.id = id;
+        this.projectTitle = projectTitle;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectTitle() {
+        return projectTitle;
+    }
+
+    public void setProjectTitle(String projectTitle) {
+        this.projectTitle = projectTitle;
+    }
 }
