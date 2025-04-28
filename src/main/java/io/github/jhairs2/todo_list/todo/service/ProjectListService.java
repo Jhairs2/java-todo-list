8f023024262d0024c3fd5c64ddf1083ce3665f25
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-28 03:10:42 -0400

    Add function for returning project lists

--- src/main/java/io/github/jhairs2/todo_list/todo/service/ProjectListService.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/service/ProjectListService.java
@@ -1,5 +1,27 @@
 package io.github.jhairs2.todo_list.todo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.jhairs2.todo_list.todo.model.ProjectList;
+import io.github.jhairs2.todo_list.todo.repository.ProjectListRepository;
+
+@Service
 public class ProjectListService {
+
+    private final ProjectListRepository projectListRepository;
+
+    @Autowired
+    public ProjectListService(ProjectListRepository projectListRepository) {
+        this.projectListRepository = projectListRepository;
+    }
+
+    public List<ProjectList> getAllProjectLists() {
+
+        return this.projectListRepository.findAll();
+
+    }
 
 }
