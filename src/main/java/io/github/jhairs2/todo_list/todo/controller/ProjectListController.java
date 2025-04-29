c21d7d9b72a1317ac4e0e8a6c28d6c8db87d798a
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-29 08:52:13 -0400

    Create GET request for returning a single project

--- src/main/java/io/github/jhairs2/todo_list/todo/controller/ProjectListController.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/controller/ProjectListController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +26,10 @@ public class ProjectListController {
     public List<ProjectListDTO> getAllProjectLists() {
         return this.projectListService.getAllProjectLists();
     }
+
+    @GetMapping(path = "/{projectListId}")
+    public ProjectListDTO getProjectList(@PathVariable("projectListId") Long id) {
+        return this.projectListService.getProjectList(id);
+    }
+
 }
