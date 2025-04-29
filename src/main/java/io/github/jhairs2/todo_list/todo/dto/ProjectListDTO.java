e1d6f50cf997eec8a859a0a187f339d38adaee48
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-29 08:04:19 -0400

    Change ProjectListDTO class to a record

--- src/main/java/io/github/jhairs2/todo_list/todo/dto/ProjectListDTO.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/dto/ProjectListDTO.java
@@ -1,28 +1,5 @@
 package io.github.jhairs2.todo_list.todo.dto;
 
-public class ProjectListDTO {
+public record ProjectListDTO(Long id, String projectTitle) {
 
-    private Long id;
-    private String projectTitle;
-
-    public ProjectListDTO(Long id, String projectTitle) {
-        this.id = id;
-        this.projectTitle = projectTitle;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getProjectTitle() {
-        return projectTitle;
-    }
-
-    public void setProjectTitle(String projectTitle) {
-        this.projectTitle = projectTitle;
-    }
 }
