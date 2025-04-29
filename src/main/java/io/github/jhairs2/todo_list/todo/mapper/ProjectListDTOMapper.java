754326ad9bdb02aeb263577e9d3bf1a3425269fa
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-29 08:25:36 -0400

    Update Mapper class with methods for converting to DTO

--- src/main/java/io/github/jhairs2/todo_list/todo/mapper/ProjectListDTOMapper.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/mapper/ProjectListDTOMapper.java
@@ -1,5 +1,25 @@
 package io.github.jhairs2.todo_list.todo.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import io.github.jhairs2.todo_list.todo.dto.ProjectListDTO;
+import io.github.jhairs2.todo_list.todo.model.ProjectList;
+
+@Component
 public class ProjectListDTOMapper {
+
+    public ProjectListDTO convertProjectToDTO(ProjectList projectList) {
+        ProjectListDTO projectListDTO = new ProjectListDTO(projectList.getId(), projectList.getListTitle());
+        return projectListDTO;
+    }
+
+    public List<ProjectListDTO> convertProjectsToDTOList(List<ProjectList> projectLists) {
+        return projectLists.stream()
+                .map(this::convertProjectToDTO)
+                .collect(Collectors.toList());
+    }
 
 }
