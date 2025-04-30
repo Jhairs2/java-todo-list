b7b9b570888e52fa7d92057322929e656c6bb00a
Justin Hairston <hairstonjustin11@yahoo.com>
2025-04-30 08:43:07 -0400

    Create methods for converting TodoItems to DTOs

--- src/main/java/io/github/jhairs2/todo_list/todo/mapper/TodoItemDTOMapper.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/mapper/TodoItemDTOMapper.java
@@ -1,5 +1,25 @@
 package io.github.jhairs2.todo_list.todo.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import io.github.jhairs2.todo_list.todo.dto.TodoItemDTO;
+import io.github.jhairs2.todo_list.todo.model.TodoItem;
+
+@Component
 public class TodoItemDTOMapper {
 
+    public TodoItemDTO convertTodoItemToDTO(TodoItem todoItem) {
+        TodoItemDTO todoDTO = new TodoItemDTO(todoItem.getId(), todoItem.getTask(), todoItem.isCompleted(),
+                todoItem.getList().getListTitle());
+        return todoDTO;
+    }
+
+    public List<TodoItemDTO> convertTodoItemsToDTOList(List<TodoItem> todoItems) {
+        return todoItems.stream()
+                .map(this::convertTodoItemToDTO)
+                .collect(Collectors.toList());
+    }
 }
