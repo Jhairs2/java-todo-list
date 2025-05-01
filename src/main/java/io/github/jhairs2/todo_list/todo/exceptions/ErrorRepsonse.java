9d4855afe88f849f27896e8e0fc489167e8b132f
Justin Hairston <hairstonjustin11@yahoo.com>
2025-05-01 08:48:03 -0400

    Create method for handling ProjectListNotFoundException

--- src/main/java/io/github/jhairs2/todo_list/todo/exceptions/ErrorRepsonse.java
+++
@@ -1,7 +0,0 @@
-package io.github.jhairs2.todo_list.todo.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public record ErrorRepsonse(String message, Throwable throwable, HttpStatus httpStatus) {
-
-}
--- src/main/java/io/github/jhairs2/todo_list/todo/exceptions/GolbalExceptionHandler.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/exceptions/GolbalExceptionHandler.java
@@ -1,5 +1,22 @@
 package io.github.jhairs2.todo_list.todo.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GolbalExceptionHandler {
+
+    @ExceptionHandler(value = ProjectListNotFoundException.class)
+    public ResponseEntity<Object> handleProjectListNotFoundException(
+            ProjectListNotFoundException projectListNotFoundException) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                projectListNotFoundException.getMessage(),
+                projectListNotFoundException.getCause(),
+                HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 
 }
