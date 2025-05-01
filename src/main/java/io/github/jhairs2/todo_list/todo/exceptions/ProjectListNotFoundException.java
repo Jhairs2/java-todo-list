c0c0b5c10a8be7ee415eb19a35cd439978978a3b
Justin Hairston <hairstonjustin11@yahoo.com>
2025-05-01 08:24:44 -0400

    Add boilerplate code to class

--- src/main/java/io/github/jhairs2/todo_list/todo/exceptions/ProjectListNotFoundException.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/exceptions/ProjectListNotFoundException.java
@@ -1,5 +1,13 @@
 package io.github.jhairs2.todo_list.todo.exceptions;
 
-public class ProjectListNotFoundException {
+public class ProjectListNotFoundException extends RuntimeException {
+
+    public ProjectListNotFoundException(String message) {
+        super(message);
+    }
+
+    public ProjectListNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
 }
