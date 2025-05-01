17920a1b04b748d1c4cffadf4c2945dc2defb7ed
Justin Hairston <hairstonjustin11@yahoo.com>
2025-05-01 09:41:40 -0400

    Remove unused imports

--- src/main/java/io/github/jhairs2/todo_list/todo/service/TodoService.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/service/TodoService.java
@@ -3,9 +3,7 @@ package io.github.jhairs2.todo_list.todo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import io.github.jhairs2.todo_list.todo.dto.TodoItemDTO;
 import io.github.jhairs2.todo_list.todo.exceptions.ProjectListNotFoundException;
