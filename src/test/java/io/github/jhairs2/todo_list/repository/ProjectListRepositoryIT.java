25e64b71b123bfb79bb03bb61f133966b083e3b7
Justin Hairston <hairstonjustin11@yahoo.com>
2025-05-05 03:20:36 -0400

    Add boilerplate code to ProjectListRepositoryIT.class

--- src/test/java/io/github/jhairs2/todo_list/repository/ProjectListRepositoryIT.java
+++ src/test/java/io/github/jhairs2/todo_list/repository/ProjectListRepositoryIT.java
@@ -1,5 +1,11 @@
 package io.github.jhairs2.todo_list.repository;
 
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class ProjectListRepositoryIT {
 
 }
