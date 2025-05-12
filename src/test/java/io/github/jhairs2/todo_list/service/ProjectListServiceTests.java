8ec1789a7a996421be52f24e9366fec17ff4f01f
Justin Hairston <hairstonjustin11@yahoo.com>
2025-05-12 03:47:05 -0400

    Fix errors in test, repo should throw exception

--- src/test/java/io/github/jhairs2/todo_list/service/ProjectListServiceTests.java
+++ src/test/java/io/github/jhairs2/todo_list/service/ProjectListServiceTests.java
@@ -107,7 +107,8 @@ public class ProjectListServiceTests {
         @DisplayName("Test should throw exception when a requested list cannot be found")
         @Test
         void shouldThrowException_IfListIsNotFound() {
-                when(this.projectListRepository.findById(1L)).thenReturn(Optional.empty());
+                when(this.projectListRepository.findById(1L))
+                                .thenThrow(new ProjectListNotFoundException("Project with that id can not be found."));
 
                 Assertions.assertThatThrownBy(() -> this.projectListService.getProjectList(1L))
                                 .isInstanceOf(ProjectListNotFoundException.class)
@@ -147,7 +148,8 @@ public class ProjectListServiceTests {
         @DisplayName("Test should throw exception when a requested list cannot be found")
         @Test
         void shouldThrowException_IfListToUpdateIsNotFound() {
-                when(this.projectListRepository.findById(1L)).thenReturn(Optional.empty());
+                when(this.projectListRepository.findById(1L))
+                                .thenThrow(new ProjectListNotFoundException("Project with that id can not be found."));
 
                 Assertions.assertThatThrownBy(() -> this.projectListService.updateProjectList(1L, this.projectList2))
                                 .isInstanceOf(ProjectListNotFoundException.class)
@@ -174,7 +176,8 @@ public class ProjectListServiceTests {
         @DisplayName("Test should throw exception when a requested list cannot be found")
         @Test
         void shouldThrowException_IfListToDeleteIsNotFound() {
-                when(this.projectListRepository.findById(1L)).thenReturn(Optional.empty());
+                when(this.projectListRepository.findById(1L))
+                                .thenThrow(new ProjectListNotFoundException("Project with that id can not be found."));
 
                 Assertions.assertThatThrownBy(() -> this.projectListService.deleteProjectList(1L))
                                 .isInstanceOf(ProjectListNotFoundException.class)
