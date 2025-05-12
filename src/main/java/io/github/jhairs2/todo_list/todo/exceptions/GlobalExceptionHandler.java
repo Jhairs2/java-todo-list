7b1f570b785cf14a2ab22c844620d68f6a90c6ea
Justin Hairston <hairstonjustin11@yahoo.com>
2025-05-12 03:49:00 -0400

    Fix typo in class name

---
+++ src/main/java/io/github/jhairs2/todo_list/todo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package io.github.jhairs2.todo_list.todo.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GolbalExceptionHandler {
+
+    @ExceptionHandler(value = { ProjectListNotFoundException.class, TodoItemNotFoundException.class })
+    public ResponseEntity<Object> handleProjectListNotFoundException(
+            Exception exception) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND,
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
