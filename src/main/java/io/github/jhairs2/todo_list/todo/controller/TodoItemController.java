87e0db4566d5f4c4cfbf44a9d4b398370eb1280a
Justin Hairston <hairstonjustin11@yahoo.com>
2025-05-14 07:25:20 -0400

    Add @ResponseStatus to addTodoToList method

--- src/main/java/io/github/jhairs2/todo_list/todo/controller/TodoItemController.java
+++ src/main/java/io/github/jhairs2/todo_list/todo/controller/TodoItemController.java
@@ -2,6 +2,7 @@ package io.github.jhairs2.todo_list.todo.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.jhairs2.todo_list.todo.dto.TodoItemDTO;
@@ -33,6 +35,7 @@ public class TodoItemController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public TodoItemDTO addTodoToList(@PathVariable("projectId") Long id, @RequestBody TodoItem task) {
         return this.todoService.addTodoToList(id, task);
     }
