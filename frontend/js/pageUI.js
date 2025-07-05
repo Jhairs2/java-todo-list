77492ae9879a0aa95d16257a874d4550028d042c
Justin Hairston <hairstonjustin11@yahoo.com>
2025-07-05 03:42:45 -0400

    Refactor and add methods for loading data to start app

--- frontend/js/pageUI.js
+++ frontend/js/pageUI.js
@@ -1,23 +1,59 @@
-import { uiHelper } from "./uiHelper.js";
+import { uiBuilder, elementsLookUp } from "./uiHelper.js";
 import { apiCalls } from "./apiCalls.js";
-import dataHelper from "./dataHelper.js";
 import eventListeners from "./events.js";
 
-const initSelectMenu = async () => {
-  try {
-    const data = await apiCalls().getProjects();
-    const projects = dataHelper().convertToArray(data);
-    uiHelper().addOptionsToSelectMenu(projects);
-  } catch (error) {
-    console.error(error);
-    uiHelper().showError(error);
-  }
+const UI = () => {
+  const api = apiCalls();
+  const ui = uiBuilder();
+  const lookUp = elementsLookUp();
+  const events = eventListeners();
+  const select = lookUp.query("#project-select");
+
+  const getProjects = async () => {
+    return await api.getProjects();
+  };
+
+  const loadProjects = async () => {
+    const projects = await getProjects();
+    ui.populateSelectMenu(projects);
+  };
+
+  // Load tasks and menu listener
+  const loadTasks = async () => {
+    // Add change listener to select menu
+    if (!events.handleSelectMenu()) {
+      events.handleSelectMenu();
+    }
+    // Select first project in menu and display it's tasks
+    select.value = select.options[1].value;
+    events.displayTasks(select.value);
+  };
+
+  const runApp = async () => {
+    try {
+      await loadProjects();
+    } catch (error) {
+      console.log(error);
+      ui.showError("Error: unable to load projects from database ):");
+      return;
+    }
+
+    try {
+      await loadTasks();
+    } catch (error) {
+      console.log(error);
+      ui.showError(
+        `Error: unable to load tasks from project ${select.value} ):`
+      );
+      return;
+    }
+
+    events.handlePageListeners();
+  };
+
+  return {
+    runApp,
+  };
 };
 
-const loadTasks = () => {
-  eventListeners().showTasksOfSelectedProject();
-  eventListeners().deleteButtonListener();
-};
-
-initSelectMenu();
-loadTasks();
+export default UI;
