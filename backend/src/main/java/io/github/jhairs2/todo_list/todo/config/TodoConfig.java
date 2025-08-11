93489b008de6485d076c52b29cd250ae2c28cdd5
Justin Hairston <hairstonjustin11@yahoo.com>
2025-08-11 07:40:16 -0400

    Update ProjectConfig with some test code

--- backend/src/main/java/io/github/jhairs2/todo_list/todo/config/TodoConfig.java
+++ backend/src/main/java/io/github/jhairs2/todo_list/todo/config/TodoConfig.java
@@ -3,26 +3,36 @@ package io.github.jhairs2.todo_list.todo.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import io.github.jhairs2.todo_list.todo.model.ProjectList;
 import io.github.jhairs2.todo_list.todo.model.TodoItem;
+import io.github.jhairs2.todo_list.todo.model.TodoUser;
 import io.github.jhairs2.todo_list.todo.repository.ProjectListRepository;
 import io.github.jhairs2.todo_list.todo.repository.TodoRepository;
+import io.github.jhairs2.todo_list.todo.repository.TodoUserRepository;
 
 @Configuration
 public class TodoConfig {
 
     @Bean
-    CommandLineRunner commandLineRunner(TodoRepository todoRepository, ProjectListRepository projectListRepository) {
+    CommandLineRunner commandLineRunner(TodoRepository todoRepository, ProjectListRepository projectListRepository,
+            TodoUserRepository todoUserRepository, PasswordEncoder encoder) {
         return args -> {
+            TodoUser user = new TodoUser("Justin", "pass");
             TodoItem item = new TodoItem("Hello!");
             ProjectList list = new ProjectList("Todos");
             ProjectList list2 = new ProjectList("Todossss");
+
             item.setList(list);
             list.getTasks().add(item);
+            user.getProjects().add(list);
+            list.setUser(user);
 
-            projectListRepository.save(list);
+            user.setPassword(encoder.encode(user.getPassword()));
+
             projectListRepository.save(list2);
+            todoUserRepository.save(user);
 
         };
 
