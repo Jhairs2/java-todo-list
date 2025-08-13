d7bbe79215f52077b99786aa6e05611e132a6314
Justin Hairston <hairstonjustin11@yahoo.com>
2025-08-13 06:28:15 -0400

    Create controller for user requests

---
+++ backend/src/main/java/io/github/jhairs2/todo_list/todo/controller/UserController.java
@@ -0,0 +1,10 @@
+package io.github.jhairs2.todo_list.todo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/accounts")
+public class UserController {
+
+}
