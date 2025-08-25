1bace07576437d31921e0717fd6edc845cbaf581
Justin Hairston <hairstonjustin11@yahoo.com>
2025-08-25 07:22:09 -0400

    Change variable names and restructure methods to validate that TodoItems belong to users and work with DTOs

--- backend/src/main/java/io/github/jhairs2/todo_list/todo/service/TodoService.java
+++ backend/src/main/java/io/github/jhairs2/todo_list/todo/service/TodoService.java
@@ -2,6 +2,8 @@ package io.github.jhairs2.todo_list.todo.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,60 +20,102 @@ import jakarta.transaction.Transactional;
 @Service
 public class TodoService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TodoService.class);
     private final TodoRepository todoRepository;
     private final TodoItemDTOMapper todoItemDTOMapper;
     private final ProjectListRepository projectListRepository;
+    private final HelperService helperService;
 
     @Autowired
     public TodoService(TodoRepository todoRepository, TodoItemDTOMapper todoItemDTOMapper,
-            ProjectListRepository projectListRepository) {
+            ProjectListRepository projectListRepository, HelperService helperService) {
         this.todoRepository = todoRepository;
         this.todoItemDTOMapper = todoItemDTOMapper;
         this.projectListRepository = projectListRepository;
+        this.helperService = helperService;
+
     }
 
-    public List<TodoItemDTO> getAllTodosFromList(Long id) {
-        ProjectList project = this.projectListRepository.findById(id)
-                .orElseThrow(() -> new ProjectListNotFoundException("Project with that id can not be found."));
+    public List<TodoItemDTO> getAllTodosFromList(Long projectListId) {
+        Long userId = this.helperService.getActiveUserId();
+        String username = this.helperService.getActiveUsername();
+
+        logger.info("Retrieving user {}'s tasks from project {}...", username, projectListId);
+        ProjectList project = this.projectListRepository
+                .findByUserIdAndId(userId, projectListId)
+                .orElseThrow(() -> new ProjectListNotFoundException(
+                        "Project with id " + projectListId + " could not be found that belongs to the user "
+                                + username));
 
         return this.todoItemDTOMapper.convertToDTOList(project.getTasks());
 
     }
 
-    public TodoItemDTO addTodoToList(Long id, TodoItem task) {
+    public TodoItemDTO addTodoToList(Long projectListId, TodoItemDTO todoItemDTO) {
+        Long userId = this.helperService.getActiveUserId();
+        String username = this.helperService.getActiveUsername();
 
-        if (task == null || task.getTask().length() == 0) {
-            throw new IllegalArgumentException("Task cannot be blank or null");
+        if (todoItemDTO == null || todoItemDTO.task().isBlank()) {
+            throw new IllegalArgumentException("TodoItem cannot be null or blank");
         }
 
-        ProjectList project = this.projectListRepository.findById(id)
-                .orElseThrow(() -> new ProjectListNotFoundException("Project with that id can not be found."));
-        task.setList(project);
-        project.getTasks().add(task);
+        TodoItem todoItem = new TodoItem(todoItemDTO.task());
 
-        return this.todoItemDTOMapper.convertToDTO(this.todoRepository.save(task));
+        logger.info("Retrieving project {} to add task '{}'", projectListId, todoItem.getTask());
+        ProjectList project = this.projectListRepository
+                .findByUserIdAndId(userId, projectListId)
+                .orElseThrow(() -> new ProjectListNotFoundException(
+                        "Project with id " + projectListId + " could not be found that belongs to the user "
+                                + username));
+        todoItem.setList(project);
+        project.getTasks().add(todoItem);
 
+        return this.todoItemDTOMapper.convertToDTO(this.todoRepository.save(todoItem));
     }
 
     @Transactional
-    public TodoItemDTO updateTodoById(Long id, TodoItem updatedTask) {
-        TodoItem oldTask = this.todoRepository.findById(id)
-                .orElseThrow(() -> new TodoItemNotFoundException("Task with that id does not exist."));
+    public TodoItemDTO updateTodoById(Long projectListId, Long todoItemId, TodoItemDTO todoItemDTO) {
+        Long userId = this.helperService.getActiveUserId();
+        String username = this.helperService.getActiveUsername();
 
-        if (updatedTask.getTask() == null) {
-            oldTask.setCompleted(updatedTask.isCompleted());
-        } else {
-            oldTask.setTask(updatedTask.getTask());
-            oldTask.setCompleted(updatedTask.isCompleted());
+        if (todoItemDTO == null) {
+            throw new IllegalArgumentException("TodoItem cannot be null");
         }
 
-        return this.todoItemDTOMapper.convertToDTO(this.todoRepository.save(oldTask));
+        logger.info("Retrieving user {}'s project {} to update task...", username, projectListId);
+        if (!this.projectListRepository.existsByUserIdAndId(userId, projectListId)) {
+            throw new ProjectListNotFoundException(
+                    "Project with id " + projectListId + " could not be found that belongs to that user "
+                            + username);
+        }
+
+        logger.info("Updating task...");
+        TodoItem newTodoItem = this.todoRepository.findByListIdAndId(projectListId, todoItemId)
+                .orElseThrow(() -> new TodoItemNotFoundException("Task with that " + todoItemId + " does not exist."));
+
+        if (todoItemDTO.task() != null && !todoItemDTO.task().isBlank()) {
+            newTodoItem.setTask(todoItemDTO.task());
+        }
+
+        newTodoItem.setCompleted(todoItemDTO.completed());
+
+        return this.todoItemDTOMapper.convertToDTO(this.todoRepository.save(newTodoItem));
 
     }
 
-    public TodoItemDTO deleteTodoFromList(Long id) {
-        TodoItem deletedTask = this.todoRepository.findById(id)
-                .orElseThrow(() -> new TodoItemNotFoundException("Task with that id does not exist."));
+    public TodoItemDTO deleteTodoFromList(Long projectListId, Long todoItemId) {
+        Long userId = this.helperService.getActiveUserId();
+        String username = this.helperService.getActiveUsername();
+        logger.info("Retrieving user {}'s project {} to delete task...", userId, projectListId);
+        if (!this.projectListRepository.existsByUserIdAndId(userId, projectListId)) {
+            throw new ProjectListNotFoundException(
+                    "Project with id " + projectListId + " could not be found that belongs to that user "
+                            + username);
+        }
+
+        logger.info("Deleting task...");
+        TodoItem deletedTask = this.todoRepository.findByListIdAndId(projectListId, todoItemId)
+                .orElseThrow(() -> new TodoItemNotFoundException("Task with that " + todoItemId + " does not exist."));
 
         this.todoRepository.delete(deletedTask);
 
