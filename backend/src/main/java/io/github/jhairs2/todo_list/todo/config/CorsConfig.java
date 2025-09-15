3585e15678e9de090980ef85ed6d073980d95b72
Justin Hairston <hairstonjustin11@yahoo.com>
2025-09-15 04:38:58 -0400

    Fix typo

--- backend/src/main/java/io/github/jhairs2/todo_list/todo/config/CorsConfig.java
+++ backend/src/main/java/io/github/jhairs2/todo_list/todo/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500",
-                        "https://jhairs2.github.io/java-todo-list)")
+                        "https://jhairs2.github.io/java-todo-list")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
