847c8de095dff7f1c04640281912262b8b067f9d
Justin Hairston <hairstonjustin11@yahoo.com>
2025-09-15 04:09:58 -0400

    Add gh-pages to CORS config

--- backend/src/main/java/io/github/jhairs2/todo_list/todo/config/CorsConfig.java
+++ backend/src/main/java/io/github/jhairs2/todo_list/todo/config/CorsConfig.java
@@ -9,7 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500",
+                        "https://jhairs2.github.io/java-todo-list)")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
